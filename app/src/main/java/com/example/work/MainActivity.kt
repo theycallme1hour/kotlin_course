c9e71cb5eaa034b99e50fc4dc0bb66b43d6e3431
theycallme1hour <irgashevamubina23>
2022-08-17 17:07:32 +0500

    first commit

--- app/src/main/java/com/example/work/MainActivity.kt
+++ app/src/main/java/com/example/work/MainActivity.kt
@@ -4,8 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var counter=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onStart() {
+super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        counter = 5+6
+        counter = 40+45
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
