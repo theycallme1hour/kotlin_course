1697f95630cad9d638b7f8d7d27218e5e0721bdd
theycallme1hour <irgashevamubina23>
2022-08-22 16:25:33 +0500

    Act

--- app/src/main/java/com/example/work/MainActivity.kt
+++ app/src/main/java/com/example/work/MainActivity.kt
@@ -11,24 +11,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(tag: "MyLog", mgs "OnCreate")
+        var tag = null
+        Log.d(tag: "MyLog", mgs "OnCreate1")
+        Log.d(tag: "MyLog", mgs "OnCreate2")
     }
     override fun onStart() {
 super.onStart()
+        var tag = null
+        Log.d(tag: "MyLog",mgs "OnStart")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d(tag: "MyLog",mgs "OnResume")
     }
 
     override fun onPause() {
         super.onPause()
         counter = 5+6
         counter = 40+45
+        var tag = null
+        Log.d(tag: "MyLog", mgs "OnPause")
     }
 
     override fun onDestroy() {
         super.onDestroy()
     }
-    class
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
 }
