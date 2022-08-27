34a7a8c548a5822c68b33f63862f90ac21482b6c
theycallme1hour <irgashevamubina23>
2022-08-27 16:37:06 +0500

    j

--- app/src/main/java/com/example/work/MainActivity.kt
+++ app/src/main/java/com/example/work/MainActivity.kt
@@ -1,49 +1,18 @@
 package com.example
 
+import android.content.Context
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.work.R
 
 
 class MainActivity : AppCompatActivity() {
-    var counter=0
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
-        Log.d(tag: "MyLog", mgs "OnCreate1")
-        Log.d(tag: "MyLog", mgs "OnCreate2")
-    }
-    override fun onStart() {
-super.onStart()
-        Log.d(tag: "MyLog",mgs "OnStart")
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(tag: "MyLog",mgs "OnResume")
     }
-
-    override fun onPause() {
-        super.onPause()
-        counter = 5+6
-        counter = 40+45
-        Log.d(tag: "MyLog", mgs "OnPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(tag: "MyLog", mgs "OnDestroy")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(tag: "MyLog", mgs "OnStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(tag: "MyLog", mgs "OnRestart")
-    }
-
 }
